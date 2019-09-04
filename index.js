1734bd81018ce208eaf974025f4ec4dbf900e166
Ly <lydialawli@gmail.com>
2019-09-04 13:02:44 +0700

    080202

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
 
+app.get('/', (req,res)=> {
+    res.send('Welcome to Airbnb API')
+})
 
-app.listen(4000, () => {
-    console.log('Ready on port 4000')
+app.listen(5000, () => {
+    console.log('Ready on port 5000')
 })

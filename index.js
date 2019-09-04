a21d63a0bb4f56b3180a73b4ed40982542edb8a1
Ly <lydialawli@gmail.com>
2019-09-04 13:12:15 +0700

    080203

--- index.js
+++ index.js
@@ -5,6 +5,39 @@ app.get('/', (req,res)=> {
     res.send('Welcome to Airbnb API')
 })
 
+app.get('/places', (req, res) => {
+    res.send({places: [
+        {
+            _id: '1',
+            title: 'Luxury Villa Indu Siam',
+            type: 'Entire Villa',
+            price: 200,
+            rating: 5,
+            city: 'Koh Samui',
+            country: 'Thailand',
+            amenities: ['Kitchen', 'Gym', 'Iron'],
+            images: [
+                'https://q-ak.bstatic.com/images/hotel/max1024x768/186/186223203.jpg',
+                'https://q-ak.bstatic.com/images/hotel/max1280x900/186/186223171.jpg'
+            ]
+        },
+        {
+            _id: '2',
+            title: 'Breathtaking Infinite Pool',
+            type: 'Entire Villa',
+            price: 180,
+            rating: 4,
+            city: 'Bali',
+            country: 'Indonesia',
+            amenities: ['Air Conditioning', 'Kitchen', 'Swimming Pool'],
+            images: [
+                'https://q-ak.bstatic.com/images/hotel/max1024x768/186/186223203.jpg',
+                'https://q-ak.bstatic.com/images/hotel/max1280x900/186/186223171.jpg'
+            ]
+        }
+    ]})
+  })
+
 app.listen(5000, () => {
     console.log('Ready on port 5000')
 })

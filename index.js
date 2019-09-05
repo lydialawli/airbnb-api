c176a68b2a781857365a8e87c4de18439bcb4c60
Ly <lydialawli@gmail.com>
2019-09-05 12:26:53 +0700

    090101

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+
 app.get('/', require('./controllers/root'))
 app.get('/places', require('./controllers/getPlaces'))
 
 
 app.listen(5000, () => {
     console.log('Ready on port 5000')
+})
+
+
+mongoose.connect('mongodb://localhost:27017/airbnb', { useNewUrlParser: true }, (err) => {
+    err ? console.log(err) : console.log('Connected to MongoDB')
 })

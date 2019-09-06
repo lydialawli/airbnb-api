590bb150f42f80cd8027e392af13460e8b6289c6
Ly <lydialawli@gmail.com>
2019-09-06 12:08:41 +0700

    090502

--- controllers/postPlaces.js
+++ controllers/postPlaces.js
@@ -2,19 +2,8 @@ const Place = require('../models/place')
 
 
 module.exports = (req, res) => {
-    Place.create({
-        title: 'Unbelievable Infinite Pool',
-        description: 'blablabla',
-        type: 'myHouse',
-        city: 'Girona',
-        country: 'Spain',
-        price: 200,
-        rating: 5,
-        guests: 6,
-        bathrooms: 2
-    }).then(data =>res.send(data))
+    Place.create(req.body)
+        .then(data => res.send(data))
         .catch(err => { console.log(err) })
 }
 
-
-  

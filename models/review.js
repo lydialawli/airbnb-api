3a40645c06f6d59181381c86a72903f1226136b4
Ly <lydialawli@gmail.com>
2019-09-09 12:56:08 +0700

    091103

---
+++ models/review.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+const Review = mongoose.model('type', {
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user',
+        required: [true, 'User is required']
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    rating: {
+        type: Number,
+        default: 0
+    },
+    content: {
+        type: String,
+        required: [true, 'Content is required']
+    },
+    place: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'place',
+        required: [true, 'Place is required']
+    },
+})
+
+module.exports = Review

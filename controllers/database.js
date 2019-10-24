0fbeef6b9fb4463e7b5b74160dc2a0c606e0a07c
lydialawli <lydialawli@gmail.com>
2019-10-24 12:57:07 +0200

    added connected time mongodb

--- controllers/database.js
+++ controllers/database.js
@@ -1,9 +1,14 @@
 const mongoose = require('mongoose')
+var time = new Date()
 
 let database = mongoose.connect(process.env.MONGODB_URI, {
+
   useNewUrlParser: true
 }, (err) => {
-  err ? console.log(err) : console.log('Connected to MongoDB')
+  err ? console.log(err) : (
+   
+    console.log(`Connected to MongoDB at ${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`)
+  )
 })
 
 module.exports = database

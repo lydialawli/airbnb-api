35418b651c133feffed5bd43f94d589274cf9676
lydialawli <lydialawli@gmail.com>
2019-10-28 16:32:23 +0100

    deep populated fav places

--- controllers/getFavorites.js
+++ controllers/getFavorites.js
@@ -1,14 +1,23 @@
 const User = require('../models/user')
 const jwt = require('jsonwebtoken')
+const Type = require('../models/type')
 
 module.exports = (req, res) => {
     let token = req.query.token
     let user = jwt.verify(token, process.env.SECRET)
 
     User.findById(user._id)
-        .populate('likes')
-        .then(user => {
-            res.send(user.likes)
+        .lean()
+        .select('likes')
+        .populate({
+            path : 'likes',
+            populate : {
+              path : 'type'
+            }
+          })
+        .then(data => {
+            res.send(data.likes)
         })
         .catch(err => console.log(err))
 }
+

7b48f610154c1bd828f9300834278442f655bbab
lydialawli <lydialawli@gmail.com>
2019-11-30 12:26:36 +0100

    access allow

--- index.js
+++ index.js
@@ -20,11 +20,11 @@ const database = require('./controllers/database')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 app.use(cors({ credentials: true, origin: process.env.APP_URL}))
-// app.all('/', function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "X-Requested-With");
-//   next()
-// });
+app.all('/', function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next()
+});
 
 // multer middleware
 const multer = require('multer')
